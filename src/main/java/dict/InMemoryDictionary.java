e69390556a0b052926653ebb1f5579c75411557a
Tamar Almogy <tamar.almogy@post.runi.ac.il>
2024-01-13 19:04:06 +0200

    Question 7

--- src/main/java/dict/InMemoryDictionary.java
+++ src/main/java/dict/InMemoryDictionary.java
@@ -1,9 +1,12 @@
 package dict;
 
+
+import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.TreeMap;
-
+import java.util.Scanner;
 /**
  * Implements a persistent dictionary that can be held entirely in memory.
  * When flushed, it writes the entire dictionary back to a file.
@@ -17,20 +20,67 @@ import java.util.TreeMap;
  */
 public class InMemoryDictionary extends TreeMap<String, String> implements PersistentDictionary {
     private static final long serialVersionUID = 1L; // (because we're extending a serializable class)
+    private File dictionaryFile; 
 
     public InMemoryDictionary(File dictFile) {
         // TODO: Implement constructor
-    }
+        super();
+        this.dictionaryFile = dictFile;
+        
+        if (!dictionaryFile.exists()) {
+            try {
+                dictionaryFile.createNewFile();
 
+            } catch (IOException e) {
+                e.printStackTrace();
+             }
+        }
+    }
+    
     @Override
     public void open() throws IOException {
         // TODO Auto-generated method stub
+        this.clear();
+        
+        try {  
+            Scanner scanner = new Scanner(this.dictionaryFile); 
+            String line;
+            
+            while (scanner.hasNextLine()) {
+                
+                line = scanner.nextLine();
+                String[] wordDef = line.split(":",2);
+                this.put(wordDef[0], wordDef[1]);
+            }
+            scanner.close();
 
-    }
+        } catch (IOException e) {
+                e.printStackTrace();
+        }
+    }  
 
     @Override
     public void close() throws IOException {
         // TODO Auto-generated method stub
 
+        try {
+            // file writer
+            FileWriter fileWriter = new FileWriter(this.dictionaryFile);
+
+            this.entrySet().forEach(entry -> {
+                try {
+                    String line = entry.getKey() + ":" + entry.getValue() + "\n";
+                    fileWriter.write(line);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
